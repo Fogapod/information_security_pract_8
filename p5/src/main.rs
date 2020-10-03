11bb42d017b999d3756d936ed0477d8a23e5a65d
Eugene <gish.ee18@gmail.com>
2020-10-03 19:09:51 +0300

    Upload practice 5

---
+++ p5/src/main.rs
@@ -0,0 +1,27 @@
+// "Fox" Cipher decoder
+
+const FOX_ENCODED: &'static str = "ВЩИТЗЬВЬЛОНЕНИУШИЗЕГАЕТСЮЕНОЙОСВЕПТПЫВЗШТРРОБОПАЕАОМНЛОЛОТМРЯЯЕЬЛОЛЕНА";
+
+fn decode_fox_string(text: &str) -> String {
+    let chars: Vec<char> = text.chars().collect();
+
+    if chars.len() % 2 != 0 {
+        panic!("Text length should be even for fox cipher");
+    }
+
+    let mut result = String::new();
+
+    let left = &chars[..chars.len() / 2];
+    let right = &chars[chars.len() / 2..];
+
+    for i in 0..left.len() {
+        result.push(left[i]);
+        result.push(right[i]);
+    }
+
+    result
+}
+
+fn main() {
+    println!("Fox cipher dexcoded: {}", decode_fox_string(FOX_ENCODED));
+}
